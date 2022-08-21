e2f838cf1f2a1a2666dcba691c94cf9eb6e2e1e1
tatekitzhak <tatekosi@gmail.com>
2022-08-21 13:01:17 +0300

    Create a useRoutes new branch from main branch

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "@/App";
 // Test import of a JavaScript module
-import { example } from '@/js/example'
+import { example } from '@/js/example';
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+import webpackLogo from '@/assets/images/webpack-logo.svg'
+
 
 // Test import of styles
-import '@/styles/index.scss'
+import '@/styles/index.scss';
+ 
 
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+ 
+const root = document.getElementById("root");
+const view = createRoot(root);
+view.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    );
 
-const heading = document.createElement('h1')
-heading.textContent = example()
 
-// Test a background image url in CSS
-const imageBackground = document.createElement('div')
-imageBackground.classList.add('image')
-
-// Test a public folder asset
-const imagePublic = document.createElement('img')
-imagePublic.src = '/assets/example.png'
-
-const app = document.querySelector('#root')
-app.append(logo, heading, imageBackground, imagePublic)
