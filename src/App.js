b66224f27da5e65b50e8ae14412e94502d9302f9
tatekitzhak <tatekosi@gmail.com>
2022-08-22 16:17:45 +0300

    Added a pages and  routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react"
+import { Routes, Route, NavLink, Link } from "react-router-dom"
+import Dashboard from '@/components/Dashboard';
 
-function App(props) {
+export const Home = () => {
+    return <div>Home page</div>
+}
+export const About = () => {
+    return <div>About </div>
+}
+export const NotFound = () => {
+    return <div>NotFound, 404 page</div>
+}
+
+function App() {
     return (
-        <div>
-            <h1>Hello App</h1>
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="dashboard">Dashboard</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="about">About</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <div className="main">
+                {/* Define all the routes */}
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="about" element={<About />}></Route>
+                    <Route path="dashboard/*" element={<Dashboard />}></Route>
+                </Routes>
+            </div>
         </div>
-    );
+    )
 }
 
 export default App;
